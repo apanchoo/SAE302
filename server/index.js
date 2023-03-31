0121a37e98dc01df02e03c5e9fbf65ee8590025d
bleach100 <debian@debian11.linuxvmimages.local>
2023-03-31 08:27:38 -0500

    Nouveau code serveur server v2

--- server/index.js
+++ server/index.js
@@ -1,74 +1,132 @@
-const app = require('express')();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const net = require("net");
+const port = 5000;
 
-const PORT = process.env.PORT || 3000;
+let clients = [];
+let chatRooms = new Map();
 
-// Stockage des utilisateurs et salons pour la démonstration
-const users = [
-  { username: 'user1', password: 'pass1' },
-  { username: 'user2', password: 'pass2' }
-];
+const server = net.createServer((socket) => {
+  clients.push(socket);
+  console.log(`Nouveau client connecté: ${socket.remoteAddress}:${socket.remotePort}`);
 
-const salons = ['sallon1', 'sallon2', 'sallon3'];
-const userRooms = new Map();
+  socket.on("data", (data) => {
+    let message = data.toString().trim();
+    handleMessage(socket, message);
+  });
 
-io.use((socket, next) => {
-  socket.on('authentication', ({ username, password }) => {
-    const user = users.find(u => u.username === username && u.password === password);
-    if (user) {
-      socket.user = user;
-      next();
-    } else {
-      socket.emit('unauthorized');
-      socket.disconnect(true);
-    }
+  socket.on("end", () => {
+    clients = clients.filter((client) => client !== socket);
+    console.log(`Client déconnecté: ${socket.remoteAddress}:${socket.remotePort}`);
+  });
+
+  socket.on("error", (err) => {
+    console.log(`Erreur: ${err}`);
   });
 });
 
-io.on('connection', socket => {
-  socket.emit('authenticated');
-  socket.emit('salons', salons);
+function handleMessage(socket, message) {
+  const [command, ...args] = message.split(" ");
 
-  socket.on('join', room => {
-    if (salons.includes(room)) {
-      socket.join(room);
-      userRooms.set(socket.id, room);
+  switch (command) {
+    case "/join":
+      joinRoom(socket, args[0]);
+      break;
+    case "/leave":
+      leaveRoom(socket);
+      break;
+    case "/msg":
+      privateMessage(socket, args[0], args.slice(1).join(" "));
+      break;
+
+    case "/nick":
+      setNickname(socket, args[0]);
+      break;
+    
+    case "/list":
+      listRooms(socket);
+      break;
+    default:
+      broadcast(socket, message);
+  }
+}
+
+function joinRoom(socket, room) {
+  if (!room) {
+    socket.write("Veuillez spécifier un nom de salon.\n");
+    return;
+  }
+
+  leaveRoom(socket);
+
+  if (!chatRooms.has(room)) {
+    chatRooms.set(room, []);
+  }
+
+  chatRooms.get(room).push(socket);
+  socket.currentRoom = room;
+  socket.write(`Vous avez rejoint le salon ${room}\n`);
+}
+
+function leaveRoom(socket) {
+  if (socket.currentRoom) {
+    chatRooms.get(socket.currentRoom).splice(chatRooms.get(socket.currentRoom).indexOf(socket), 1);
+    socket.currentRoom = null;
+  }
+}
+
+function privateMessage(socket, target, message) {
+  const targetSocket = clients.find((client) => client.nickname === target);
+
+  if (!targetSocket) {
+    socket.write("Utilisateur introuvable.\n");
+    return;
+  }
+
+  if (!message) {
+    socket.write("Veuillez entrer un message.\n");
+    return;
+  }
+
+  targetSocket.write(`${socket.nickname} (privé): ${message}\n`);
+}
+
+function broadcast(socket, message) {
+  if (!socket.currentRoom) {
+    socket.write("Veuillez rejoindre un salon pour envoyer des messages.\n");
+    return;
+  }
+
+  chatRooms.get(socket.currentRoom).forEach((client) => {
+    if (client === socket) {
+      client.write(`Vous: ${message}\n`);
     } else {
-      socket.emit('error', `Salon ${room} introuvable`);
+      client.write(`${socket.nickname}: ${message}\n`);
     }
   });
+}
 
-  socket.on('message', ({ room, text }) => {
-    if (socket.rooms.has(room)) {
-      io.to(room).emit('message', { user: socket.user.username, text });
-    } else {
-      socket.emit('error', `Vous n'êtes pas dans le salon ${room}`);
-    }
-  });
 
-  socket.on('list_users', (room, callback) => {
-    if (salons.includes(room)) {
-      const clients = io.sockets.adapter.rooms.get(room);
-      const usersInRoom = Array.from(clients)
-        .map(clientId => io.sockets.sockets.get(clientId).user.username);
-      callback(usersInRoom);
-    } else {
-      socket.emit('error', `Salon ${room} introuvable`);
-    }
-  });
+function listRooms(socket) {
+  const rooms = Array.from(chatRooms.keys()).join(", ");
+  socket.write(`Salons disponibles: ${rooms}\n`);
+}
 
-  socket.on('private_message', ({ user, text }) => {
-    const recipientSocket = Array.from(io.sockets.sockets.values())
-      .find(s => s.user.username === user);
-    if (recipientSocket) {
-      recipientSocket.emit('private_message', { user: socket.user.username, text });
-    } else {
-      socket.emit('error', `Utilisateur ${user} introuvable`);
-    }
-  });
-});
 
-http.listen(PORT, () => {
-  console.log(`Serveur en écoute sur le port ${PORT}`);
+function setNickname(socket, nickname) {
+  if (!nickname) {
+    socket.write("Veuillez spécifier un pseudo.\n");
+    return;
+  }
+
+  if (clients.some((client) => client.nickname === nickname)) {
+    socket.write("Ce pseudo est déjà utilisé. Veuillez en choisir un autre.\n");
+    return;
+  }
+
+  socket.nickname = nickname;
+  socket.write(`Votre pseudo est maintenant ${nickname}\n`);
+}
+
+
+server.listen(port, () => {
+  console.log(`Serveur de chat en écoute sur le port ${port}`);
 });
